3fae24a64eaf90f2cdbd6f02afd60f9704b1e5ef
Fanny Simon <contact.fanny.simon@gmail.com>
2025-03-06 16:22:44 +0100

    classList.add, todos remove, Local storage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- // Créer un form pour taper les todos
+// Créer un form pour taper les todos
 
 // Ajouter sur le DOM ce qui est tapé dans l'input à la validation du form
 // monInput.value
@@ -10,22 +10,41 @@
 
 // Consulter le local storage au lancement de l'application pour rajouter des todos
 
+// -----------------------------------------------------------------------------------------------------------------
 
- const input = document.getElementById("input");
- const form = document.querySelector("form");
- const ul = document.querySelector("ul");
- 
+const item = document.getElementById("item"); //  id de l'input
+const form = document.querySelector("form");
+const list = document.getElementById("list"); //  id de l'ul
+
+// Storage part
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo pour la supprimer</li>`;
+  }
+}
+
+window.addEventListener("load", getTodos());
+
 //  Evenement au submit sur le formulaire
- form.addEventListener("submit", function (e) {
-     e.preventDefault();
-     const inputValue = input.value;
-     ul.innerHTML += `<li>${input.value}</li>`;
-     input.value = "";
-   });
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+  storeList();
+});
 
-  //  Evènement au click sur le todo
-  
-
-
-
- 
+//  Evènement au click sur le todo
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
